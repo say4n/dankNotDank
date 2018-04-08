9d43c1b7dd4d5590a8e3b040664becedd7159836
Sayan Goswami <goswami.sayan47@gmail.com>
2018-04-08 19:57:18 +0530

    Fixes for wget
    ?

--- src/data/downloader.py
+++ src/data/downloader.py
@@ -2,9 +2,12 @@
 
 from decouple import config
 from multiprocessing import Pool
+import os
+import shutil
 import subprocess
 from tinydb import TinyDB, Query
-import wget
+from urllib.request import urlopen
+from urllib.parse import urlparse
 
 
 # Data
@@ -26,18 +29,26 @@ meme_urls = map(lambda submission: (submission["id"], submission["media"]),
 
 # Config
 NUM_WORKERS = 8
+ITEM_Q = []
 
 
 def download(post_id, post_url, output_dir=OUTPUT_DIR):
     """Given an image url, save it to output_dir"""
-    filename = wget.download(post_url, out=output_dir)
+    global ITEM_Q
+
+    filename = os.path.basename(urlparse(post_url).path)
+    output_filename = os.path.join(output_dir, filename)
+    
+    # Download the file from `url` and save it locally under `output_filename`:    
+    with urlopen(post_url) as response, open(output_filename, 'wb') as out_file:
+        shutil.copyfileobj(response, out_file)
 
     Post = Query()
     items = db.search(Post.id == post_id)
     item = items[0]
     item["filename"] = filename
 
-    meme_db.insert(item)
+    ITEM_Q.append(item)
     
     print(f"Downloaded {filename}")
 
@@ -47,3 +58,6 @@ if __name__ == "__main__":
 
     with Pool(NUM_WORKERS) as p:
         p.starmap(download, meme_urls)
+
+    for item in ITEM_Q:
+        meme_db.insert(item)
