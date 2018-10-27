10540f699fa1e61ba10cbb442a5d75b88a0af2fa
Sayan Goswami <goswami.sayan47@gmail.com>
2018-10-27 22:00:50 +0530

    Add viz
    Distribution of upvotes

---
+++ src/data/viz.py
@@ -0,0 +1,25 @@
+from decouple import config
+import matplotlib.pyplot as plt
+import numpy as np
+from tinydb import TinyDB, Query
+
+
+# Database
+PATH = config("DATABASE_PATH")
+db = TinyDB(PATH)
+
+data = db.all()
+
+upvotes = [record['ups'] for record in data]
+
+
+hist, bins, _ = plt.hist(upvotes)
+plt.clf()
+logbins = np.logspace(np.log10(bins[0]),np.log10(bins[-1]),len(bins))
+plt.hist(upvotes, bins=logbins)
+
+plt.ylabel("# of Posts")
+plt.xlabel("# of Upvotes")
+plt.title("Distribution of Upvotes")
+
+plt.show()
